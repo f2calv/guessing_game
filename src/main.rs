1f566b38e9496e172bdbb1b539e5af9f8640bddd
Alex Vincent <alexv@microsoft.com>
2022-10-10 12:45:48 +0000

    handle input

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,20 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess...");
 
-    let mut guess = String::new();
+    //let mut guess = String::new();
+    let mut guess;
+    guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    // match io::stdin().read_line(&mut guess) {
+    //     Ok(n) => {
+    //         println!("{n} bytes read");
+    //         println!("{guess}");
+    //     }
+    //     Err(error) => println!("error: {error}"),
+    // }
 
     println!("You guessed: {guess}");
 }
