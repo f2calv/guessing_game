b12320f08445ae7862a7ff81f55db84a8e6ed3aa
Alex Vincent <alexv@microsoft.com>
2022-10-11 13:44:40 +0000

    basic functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+#![allow(unused)]
+
 fn main() {
     println!("Hello, world!");
+
+    another_function(4, 'w');
+    //let x = (let y = 6);
+
+    let x = {
+        let y = 6;
+        y + 1 + 1_0_0_0_0_0_0
+    };
+
+    println!("the value of x is: {x}")
+}
+
+fn another_function(x: u32, unit_label: char) {
+    println!("The value of x is: {x}, char is: {unit_label}");
 }
