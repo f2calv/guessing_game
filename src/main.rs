4269624eb5a105015402b242aee3c0bd7f662bd8
Alex Vincent <alexv@microsoft.com>
2022-10-11 13:48:53 +0000

    statement vs expression

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,25 @@ fn main() {
         y + 1 + 1_0_0_0_0_0_0
     };
 
-    println!("the value of x is: {x}")
+    println!("the value of x is: {x}");
+
+    {
+        let x = five();
+        println!("the value of x is: {x}");
+
+        let y = plus_one(x);
+        println!("the value of y is: {y}");
+    }
 }
 
 fn another_function(x: u32, unit_label: char) {
     println!("The value of x is: {x}, char is: {unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(value: i32) -> i32 {
+    value + 1
 }
