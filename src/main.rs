a8bcd6b77c7168d502671e475f75e953e9c955be
Alex Vincent <alexv@microsoft.com>
2022-10-11 14:18:00 +0000

    loops

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
 #![allow(unused)]
 
 fn main() {
-    println!("Hello, world!");
+    if_conditions();
+    try_loops();
+}
+
+fn try_loops() {
+    let mut i = 0;
+    loop {
+        if (i % 1000 == 0) {
+            println!("hey! {i}");
+        }
+        i += 1;
+    }
+}
+
+fn if_conditions() {
+    let number = 33;
+
+    if number < 5 {
+        println!("condition was true");
+    } else if (number == 10) {
+        //do nothing
+    } else if (number % 3 == 0) {
+        println!("number was divisible by 3");
+    } else {
+        println!("condition was false");
+    }
+
+    let condition = true;
+
+    let number = if condition { 5 } else { 6 };
+    println!("the value of number is: {number}");
 }
