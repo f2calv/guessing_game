e4b2554c8d4838fdaaff6d661fb21e456dec89f5
Alex Vincent <alexv@microsoft.com>
2022-10-11 13:50:25 +0000

    comments

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,18 @@ fn main() {
         println!("the value of x is: {x}");
 
         let y = plus_one(x);
-        println!("the value of y is: {y}");
+        println!("the value of y is: {y}"); //this is a statement (ends with semi-colon)
     }
 }
 
+//here is a really long comment which needs
+//to be multiline
 fn another_function(x: u32, unit_label: char) {
     println!("The value of x is: {x}, char is: {unit_label}");
 }
 
 fn five() -> i32 {
-    5
+    5 //this is an expression
 }
 
 fn plus_one(value: i32) -> i32 {
