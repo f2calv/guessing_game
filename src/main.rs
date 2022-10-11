440cf6ac36ae7624ad959c6bec0bf11c685cbb42
Alex Vincent <alexv@microsoft.com>
2022-10-11 14:23:01 +0000

    ret val

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 fn main() {
     try_loops();
+    let result = try_loops_ret_value();
+    println!("loop result is {result}");
 }
 
 fn try_loops() {
@@ -11,5 +13,23 @@ fn try_loops() {
             println!("hey! {i}");
         }
         i += 1;
+
+        if i > 1_000_000 {
+            break;
+        }
+    }
+}
+
+fn try_loops_ret_value() -> i32 {
+    let mut i = 0;
+    loop {
+        if (i % 1000 == 0) {
+            println!("hey! {i}");
+        }
+        i += 1;
+
+        if i > 1_000_000 {
+            break i * 2_000;
+        }
     }
 }
