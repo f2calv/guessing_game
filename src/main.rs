f61fcfe43cd6af28b6e3a9e0973d83bad9096d7a
Alex Vincent <alexv@microsoft.com>
2022-10-15 06:42:35 +0000

    fail

--- src/main.rs
+++ src/main.rs
@@ -59,6 +59,21 @@ fn main() {
         change(&mut s);
         println!("{s}");
     }
+
+    {
+        let mut s = String::from("hello");
+        let r1 = &mut s;
+        //let r2 = &mut s;
+        //println!("{r1}, {r2}");
+    }
+
+    {
+        let mut s = String::from("hello");
+        let r1 = &s;
+        let r2 = &s;
+        let r3 = &mut s;
+        println!("{r1}, {r2}, {r3}");
+    }
 }
 
 fn change(some_string: &mut String) {
